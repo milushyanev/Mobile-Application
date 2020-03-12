6a98974961384ebda2cc44d2000d8686cfcb8a81
M Yanev <myanev@Mpowers.Allienware>
2020-03-11 18:22:02 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'secondPage.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      home: new LoginPage(),
+      theme: new ThemeData(
+        primarySwatch: Colors.green
+      )
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget{
+  @override
+  State createState() => new LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> with TickerProviderStateMixin{
+
+  AnimationController _iconAnimationController;
+  Animation<double> _iconAnimation;
+
+  @override
+  void initState(){
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        vsync: this,
+        duration: new Duration(microseconds: 500)
+    );
+    _iconAnimation=new CurvedAnimation(
+        parent: _iconAnimationController,
+        curve: Curves.easeOut
+    );
+    _iconAnimation.addListener(()=>this.setState((){}));
+    _iconAnimationController.forward();
+  }
+  Widget build(BuildContext context){
+    return new Scaffold(
+      //backgroundColor: Colors.blueAccent,
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Image(
+              image: AssetImage("assets/plogo.jpg"),
+          fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode:BlendMode.darken,
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(
+                size: _iconAnimation.value * 100,
+              ),
+              new Form(
+                child: new Theme(
+                  data: new ThemeData(
+                    brightness: Brightness.dark,
+                    primarySwatch: Colors.teal,
+                    inputDecorationTheme: new InputDecorationTheme(
+                      labelStyle: new TextStyle(
+                        color: Colors.teal, fontSize: 20.0
+                      )
+                    )
+                  ),
+                  child: new Container(
+                    padding: const EdgeInsets.all(40.0),
+                    child: new Column(
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Email Address",
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "Password",
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        new Padding(
+                            padding: const EdgeInsets.only(
+                                top: 20.0),
+                        ),
+                        new MaterialButton(
+                          height: 50.0,
+                          minWidth: 100.0,
+                          color: Colors.teal,
+                          textColor: Colors.white,
+                          child: new Text(
+                              "Login"),
+                          onPressed: ()=>{
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SecondRoute()),
+                          ),
+                        },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/secondPage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.black,
+
+    );
+  }
+}
