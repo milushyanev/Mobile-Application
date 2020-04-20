e50309982d87dee4d96be2b0842832252f3a1639
myanev <milushyanev@yahoo.com>
2020-04-19 20:57:17 -0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/signUp.dart';
 import 'secondPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'signUp.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
   @override
+
   Widget build(BuildContext context){
     return new MaterialApp(
       home: new LoginPage(),
@@ -19,16 +23,28 @@ class MyApp extends StatelessWidget{
 class LoginPage extends StatefulWidget{
   @override
   State createState() => new LoginPageState();
+
 }
+
 
 class LoginPageState extends State<LoginPage> with TickerProviderStateMixin{
 
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  var emailEditingController= TextEditingController();
+  var passEditingController= TextEditingController();
+
+
+  bool error = false;
+  String errorMessage="";
   AnimationController _iconAnimationController;
   Animation<double> _iconAnimation;
 
-  @override
   void initState(){
+
     super.initState();
+
     _iconAnimationController = new AnimationController(
         vsync: this,
         duration: new Duration(microseconds: 500)
@@ -39,97 +55,136 @@ class LoginPageState extends State<LoginPage> with TickerProviderStateMixin{
     );
     _iconAnimation.addListener(()=>this.setState((){}));
     _iconAnimationController.forward();
+
+
+
   }
   Widget build(BuildContext context){
+
+
     return new Scaffold(
+      //resizeToAvoidBottomInset: false,
       //backgroundColor: Colors.blueAccent,
       body: new Stack(
         fit: StackFit.expand,
         children: <Widget>[
+          Padding(padding: EdgeInsets.all(10.0)),
           new Image(
               image: AssetImage("assets/plogo.jpg"),
           fit: BoxFit.cover,
             color: Colors.black87,
             colorBlendMode:BlendMode.darken,
           ),
-          new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+
+          ListView(
             children: <Widget>[
-              new FlutterLogo(
-                size: _iconAnimation.value * 65,
-              ),
-              new Form(
-                child: new Theme(
-                  data: new ThemeData(
-                    brightness: Brightness.dark,
-                    primarySwatch: Colors.teal,
-                    inputDecorationTheme: new InputDecorationTheme(
-                      labelStyle: new TextStyle(
-                        color: Colors.teal, fontSize: 20.0
-                      )
-                    )
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.all(50.0)),
+                  new FlutterLogo(
+                    size: _iconAnimation.value * 65,
                   ),
-                  child: new Container(
-                    padding: const EdgeInsets.all(40.0),
-                    child: new Column(
-                      children: <Widget>[
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Email Address",
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Password",
-                          ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        new Padding(
-                            padding: const EdgeInsets.only(
-                                top: 20.0),
-                        ),
-                        new MaterialButton(
-                          height: 50.0,
-                          minWidth: 100.0,
-                          color: Colors.teal,
-                          textColor: Colors.white,
-                          child: new Text(
-                              "Login"),
-                          onPressed: ()=>{
-                          Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => SecondRoute()),
-                          ),
-                        },
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(bottom: 10),
+                  new Form(
+                    child: new Theme(
+                      data: new ThemeData(
+                          brightness: Brightness.dark,
+                          primarySwatch: Colors.teal,
+                          inputDecorationTheme: new InputDecorationTheme(
+                              labelStyle: new TextStyle(
+                                  color: Colors.teal, fontSize: 20.0
+                              )
+                          )
+                      ),
+                      child: new Container(
+                        padding: const EdgeInsets.all(40.0),
+                        child: new Column(
+                          children: <Widget>[
+                            new TextFormField(
+                              controller: emailEditingController,
+                              decoration: new InputDecoration(
+                                labelText: "Email Address",
+                              ),
+                              keyboardType: TextInputType.emailAddress,
                             ),
-                        new MaterialButton(
-
-                          height: 50.0,
-                          minWidth: 100.0,
-                          color: Colors.teal,
-                          textColor: Colors.white,
-
-                          child: new Text(
-                              "SignUp"),
-                          onPressed: ()=>{
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => SignUpPage()),
+                            error ?
+                            new Text(errorMessage,style:TextStyle(color: Colors.red)) : Padding(padding: EdgeInsets.all(0)),
+                            new TextFormField(
+                              controller: passEditingController,
+                              decoration: new InputDecoration(
+                                labelText: "Password",
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
                             ),
-                          },
-                        )
-                      ],
+                            new Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 20.0),
+                            ),
+                            new MaterialButton(
+                              height: 50.0,
+                              minWidth: 100.0,
+                              color: Colors.teal,
+                              textColor: Colors.white,
+                              child: new Text(
+                                  "Login"),
+                              onPressed: ()=>{
+                                setState((){
+                                  this.error=false;
+
+                                }),
+
+                                _auth.signInWithEmailAndPassword(email: emailEditingController.text.toString(),
+                                    password: passEditingController.text.toString()).then((value){
+                                  print("Success" + value.user.uid);
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()));
+
+                                }).catchError((e){
+                                  print("Failed" + e.toString());
+                                  setState(() {
+                                    this.error=true;
+                                    this.errorMessage = e.toString();
+                                  });
+
+
+                                })
+                              },
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 10),
+                            ),
+
+                            new MaterialButton(
+
+                              height: 50.0,
+                              minWidth: 100.0,
+                              color: Colors.teal,
+                              textColor: Colors.white,
+
+                              child: new Text(
+                                  "SignUp"),
+                              onPressed: ()=>{
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => SignUpPage()),
+                                ),
+                              },
+
+                            ),
+
+
+                          ],
+
+                        ),
+
+
+                      ),
                     ),
-                  ),
-                ),
+                  )
+                ],
               )
             ],
-          )
+          ),
         ],
       ),
     );
--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(new MyApp());
 
@@ -24,6 +26,19 @@ class SignUpPageState extends State<SignUpPage> with TickerProviderStateMixin{
 
   AnimationController _iconAnimationController;
   Animation<double> _iconAnimation;
+  bool error = false;
+
+  String errorMessage="";
+
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  var emailEditingController= TextEditingController();
+  var passEditingController= TextEditingController();
+  var firstNameEditingController= TextEditingController();
+  var lastNameEditingController= TextEditingController();
+  var ageEditingController= TextEditingController();
+
 
   @override
   void initState(){
@@ -38,9 +53,11 @@ class SignUpPageState extends State<SignUpPage> with TickerProviderStateMixin{
     );
     _iconAnimation.addListener(()=>this.setState((){}));
     _iconAnimationController.forward();
+
   }
   Widget build(BuildContext context){
     return new Scaffold(
+      //resizeToAvoidBottomInset: false,
       //backgroundColor: Colors.blueAccent,
       body: new Stack(
         fit: StackFit.expand,
@@ -51,76 +68,139 @@ class SignUpPageState extends State<SignUpPage> with TickerProviderStateMixin{
             color: Colors.black87,
             colorBlendMode:BlendMode.darken,
           ),
-          new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          ListView(
             children: <Widget>[
-              new FlutterLogo(
-                size: _iconAnimation.value * 50,
-              ),
-              new Form(
-                child: new Theme(
-                  data: new ThemeData(
-                      brightness: Brightness.dark,
-                      primarySwatch: Colors.teal,
-                      inputDecorationTheme: new InputDecorationTheme(
-                          labelStyle: new TextStyle(
-                              color: Colors.teal, fontSize: 20.0
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new FlutterLogo(
+                    size: _iconAnimation.value * 50,
+                  ),
+                  new Form(
+                    child: new Theme(
+                      data: new ThemeData(
+                          brightness: Brightness.dark,
+                          primarySwatch: Colors.teal,
+                          inputDecorationTheme: new InputDecorationTheme(
+                              labelStyle: new TextStyle(
+                                  color: Colors.teal, fontSize: 20.0
+                              )
                           )
-                      )
-                  ),
-                  child: new Container(
-                    padding: const EdgeInsets.all(40.0),
-                    child: new Column(
-                      children: <Widget>[
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Email Address",
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Password",
-                          ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: "Re-Enter Password",
-                          ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        new Padding(
-                          padding: const EdgeInsets.only(
-                              top: 20.0),
-                        ),
-                        new MaterialButton(
-                          height: 50.0,
-                          minWidth: 100.0,
-                          color: Colors.teal,
-                          textColor: Colors.white,
-                          child: new Text(
-                              "Sign Up"),
-                          onPressed: ()=>{
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => LoginPage()),
+                      ),
+                      child: new Container(
+                        padding: const EdgeInsets.all(40.0),
+                        child: new Column(
+                          children: <Widget>[
+                            new TextFormField(
+                              controller: emailEditingController,
+                              decoration: new InputDecoration(
+                                labelText: "Email Address",
+                              ),
+                              keyboardType: TextInputType.emailAddress,
                             ),
-                          },
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(bottom: 10),
-                        ),
+                            error ?
+                            new Text(errorMessage,style:TextStyle(color: Colors.red)) : Padding(padding: EdgeInsets.all(0)),
+                            new TextFormField(
+                              controller: passEditingController,
+                              decoration: new InputDecoration(
+                                labelText: "Password",
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                            ),
+                            new TextFormField(
+                              controller: firstNameEditingController,
+                              decoration: new InputDecoration(
+                                labelText: "First Namer",
+                              ),
+                              keyboardType: TextInputType.text,
 
-                      ],
+                            ),
+                            new TextFormField(
+                              controller: lastNameEditingController,
+                              decoration: new InputDecoration(
+                                labelText: "Last Name",
+                              ),
+                              keyboardType: TextInputType.text,
+
+                            ),
+                            new TextFormField(
+                              controller: ageEditingController,
+                              decoration: new InputDecoration(
+                                labelText: "Age",
+                              ),
+                              keyboardType: TextInputType.text,
+
+                            ),
+
+
+                            new Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 20.0),
+                            ),
+                            new MaterialButton(
+
+                              height: 50.0,
+                              minWidth: 100.0,
+                              color: Colors.teal,
+                              textColor: Colors.white,
+
+                              child: new Text(
+                                  "SignUp"),
+                              onPressed: ()=>{
+                                setState((){
+                                  this.error=false;
+
+                                }),
+
+                                print(emailEditingController.text.toString()),
+
+                                print(passEditingController.text.toString()),
+
+                                _auth.createUserWithEmailAndPassword(email:
+                                emailEditingController.text.toString(),
+                                    password: passEditingController.text.toString())
+                                    .then((value) async {
+                                  String lname = lastNameEditingController.text.toString();
+                                  String fname = firstNameEditingController.text.toString();
+                                  String age=ageEditingController.text.toString();
+                                  String email = emailEditingController.text.toString();
+                                  final CollectionReference usersCollection = Firestore.instance.collection('users');
+                                  await usersCollection.document(value.user.uid).setData({
+                                    'fname' : fname,
+                                    'lname' : lname,
+                                    'age'   : age,
+                                    'email' : email,
+                                    'timestamp': DateTime.now().toUtc().millisecondsSinceEpoch
+
+
+                                  });
+
+                                })
+                                    .catchError((error) {
+                                  setState(() {
+                                    this.error = true;
+                                    this.errorMessage = error.toString();
+                                  });
+                                }),
+
+
+                              },
+
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 10),
+                            ),
+
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                  )
+                ],
               )
             ],
-          )
+          ),
         ],
       ),
     );
